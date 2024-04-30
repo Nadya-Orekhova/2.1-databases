021cf5c7caf3fe438d4f472a841c3e84d8424569
Nadya Orekhova <ninilavaet@gmail.com>
2024-04-30 22:13:05 +0300

    add work_with_database/

---
+++ work_with_database/phones/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from django.template.defaultfilters import slugify
+
+
+class Phone(models.Model):
+    name = models.CharField(max_length=40)
+    price = models.IntegerField()
+    image = models.URLField()
+    release_date = models.DateField()
+    lte_exists = models.BooleanField()
+    slug = models.SlugField()
+
+
+    def __str__(self):
+        return f'{self.name}, {self.image}, {self.image}, {self.release_date}, {self.lte_exists}'
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        return super().save(*args, **kwargs)
+
+
---
+++ work_with_database/phones/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render, redirect
+from phones.models import Phone
+
+def index(request):
+    return redirect('catalog')
+
+
+def show_catalog(request):
+    sort_param = request.GET.get('sort', 'name')
+    template = 'catalog.html'
+    sort_dict = {'name': 'name', 'min_price': 'price', 'max_price': '-price'}
+    phones = Phone.objects.all().order_by(sort_dict[sort_param])
+    context = {'phones': phones}
+    return render(request, template, context)
+
+
+def show_product(request, slug):
+    template = 'product.html'
+    phone = Phone.objects.get(slug=slug)
+    context = {'phone': phone}
+    print(phone.name)
+    return render(request, template, context)
+
